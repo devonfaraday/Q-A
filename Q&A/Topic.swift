d5a631bcd2eec23e7c009b5140eb8f0a935fef3c
Hayden32 <haydenhastings@gmail.com>
2017-04-10 17:17:03 -0600

    topic model

--- Q&A/Topic.swift
+++ Q&A/Topic.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import CloudKit
+
+class Topic {
+    
+    static let typeKey = "Q&A"
+    static let nameKey = "name"
+    static let codeGeneratorKey = "codeGenerator"
+    static let questionsKey = "questions"
+    
+    let name: String
+    let codeGenerator: Int
+    let questions: [Question]
+    let recordID: CKRecordID?
+    
+    init(name: String, codeGenerator: Int, questions: [Question] = [], recordID: CKRecordID) {
+        self.name = name
+        self.codeGenerator = codeGenerator
+        self.questions = questions
+        self.recordID = recordID
+    }
+    
+    // MARK: - CloudKit Syncable
+    
+    var recordType: String {
+        return Topic.typeKey
+    }
+    
+    convenience required init?(record: CKRecord) {
+        guard let name = record[Topic.nameKey] as? String,
+            let codeGenerator = record[Topic.codeGeneratorKey] as? Int,
+            let questions = record[Topic.questionsKey] as? [String]
+            else { return nil }
+        
+        self.init(name: name, codeGenerator: codeGenerator, questions: questions, recordID: record.recordID)
+        
+    }
+}
+
+extension CKRecord {
+    
+    convenience init(topic: Topic) {
+        
+        let recordID = topic.recordID ?? CKRecordID(recordName: UUID().uuidString)
+        self.init(recordType: "Q&A", recordID: recordID)
+        self.setValue(topic.name, forKey: Topic.nameKey)
+        self.setValue(topic.codeGenerator, forKey: Topic.codeGeneratorKey)
+        self.setValue(topic.questions, forKey: Topic.questionsKey)
+    }
+}
