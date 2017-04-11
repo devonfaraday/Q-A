0ae8e971a769ff6aaa354b3f5ca88979c60a1342
Hayden32 <haydenhastings@gmail.com>
2017-04-11 11:33:12 -0600

    Working on saving Topic

--- Q&A/TopicController.swift
+++ Q&A/TopicController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import CloudKit
+
+class TopicController {
+    
+    var topics: [Topic] = []
+    static let shared = TopicController()
+    let cloudKitManager = CloudKitManager()
+    
+    func createTopic(name: String,  questions: [Question], recordID: CKRecordID, completion: @escaping (Error?) -> Void) {
+        let randomNum = randomNum()
+        let topics = Topic(name: name, codeGenerator: randomNum, questions: questions, recordID: recordID)
+        
+        let record = CKRecord(topic: topics)
+        cloudKitManager.publicDatabase.save(record) { (_, error) in
+            
+            if let error = error {
+                print("There was an error saving to CloudKit. TopicController: createTopic()")
+                completion(error)
+            } else {
+                print("Record successfully saved to CloudKit")
+                self.topics.append(topics)
+                completion(nil)
+            }
+        }
+    }
+    
+    func fetchTopicFromCloudKit(completion: @escaping ([Topic]) -> Void) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Topic", predicate: predicate)
+        
+        cloudKitManager.publicDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            guard let records = records
+                else { return }
+            let topics = records.flatMap({ Topic(record: $0)})
+            completion(topics)
+        }
+    }
+    
+    func randomNum() -> Int {
+        var arrayOfNumbers = []
+    }
+}
