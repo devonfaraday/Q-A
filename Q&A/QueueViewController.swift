fa90c03c258fde0fd7ad6233bfd4d479948e1d41
demickm <demickm@hotmail.com>
2017-04-12 15:13:31 -0600

    preparing for another pull from develope

--- Q&A/QueueViewController.swift
+++ Q&A/QueueViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import CloudKit
 
-class QueueViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class QueueViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
-    var topic: Topic?
+    var topic: Topic? {
+        didSet {
+            updateView()
+        }
+    }
     
     // MARK: - IBOutlets
     
@@ -24,13 +28,25 @@ class QueueViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var codeLabel: UILabel!
     @IBOutlet weak var readyButton: UIButton!
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // function Completion
+        topicNameTextField.borderStyle = .none
+        topicNameTextField.isEnabled = false
+        topicNameTextField.resignFirstResponder()
+        
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewTypeSetup()
         showTopicNumber()
         questionTableView.reloadData()
     }
     
+    func updateView() {
+      
+    }
     // MARK: - Data Source Functions
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,12 +70,18 @@ class QueueViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
+    
     func viewTypeSetup() {
         guard let topic = topic, let currentUser = TopicController.shared.currentUser else {return}
-        if topic.topicOwner == currentUser.recordID {
-            print ("True")
+        if topic.topicOwner.recordID == currentUser.recordID {
+           
+            readyButton.isHidden = true
+            askQuestionButton.isHidden = true
         } else {
-            print ("FUCK")
+            blockButton.isHidden = true
+            readyCheckButton.isHidden = true
+            clearButton.isHidden = true
+   
         }
     }
     
