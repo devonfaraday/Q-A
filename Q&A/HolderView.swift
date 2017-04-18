8f5575127423446704edec369d2dbeb6b2178357
Hayden32 <haydenhastings@gmail.com>
2017-04-18 17:10:34 -0600

    LauchScreen

---
+++ Q&A/HolderView.swift
@@ -0,0 +1,76 @@
+//
+//  HolderView.swift
+//  Q&A
+//
+//  Created by Hayden Hastings on 4/18/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class HolderView: UIView {
+    weak var delegate: HolderViewDelegate?
+    var parentFrame: CGRect = CGRect.zero
+    
+    let blueRectangleLayer = RectangleLayer()
+    
+    func expandView() {
+        // 1
+        backgroundColor = UIColor.blue
+        // 2
+        frame = CGRect(x: frame.origin.x - blueRectangleLayer.lineWidth, y: frame.origin.y - blueRectangleLayer.lineWidth, width: blueRectangleLayer.lineWidth * 2, height: blueRectangleLayer.lineWidth * 2)
+        
+        // 3
+        layer.sublayers = nil
+        
+        // 4
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut,
+                       animations: { self.frame = self.parentFrame
+        }, completion: { finished in
+            self.addLabel()
+        })
+    }
+    
+    func addLabel() {
+        delegate?.animateLabel()
+    }
+
+}
+
+protocol HolderViewDelegate: class {
+    func animateLabel()
+}
+
+class RectangleLayer: CAShapeLayer {
+    
+    override init() {
+        super.init()
+        fillColor = UIColor.clear.cgColor
+        lineWidth = 5.0
+        path = rectanglePathFull.cgPath
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var rectanglePathFull: UIBezierPath {
+        let rectanglePath = UIBezierPath()
+        rectanglePath.move(to: CGPoint(x: 0.0, y: 100.0))
+        rectanglePath.addLine(to: CGPoint(x: 0.0, y: -lineWidth))
+        rectanglePath.addLine(to: CGPoint(x: 100.0, y: -lineWidth))
+        rectanglePath.addLine(to: CGPoint(x: 100.0, y: 100.0))
+        rectanglePath.addLine(to: CGPoint(x: -lineWidth / 2, y: 100.0))
+        rectanglePath.close()
+        return rectanglePath
+    }
+    
+    func animateStrokeWithColor(_ color: UIColor) {
+        strokeColor = color.cgColor
+        let strokeAnimation: CABasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        strokeAnimation.fromValue = 0.0
+        strokeAnimation.toValue = 1.0
+        strokeAnimation.duration = 0.4
+        add(strokeAnimation, forKey: nil)
+    }
+}
--- Q&A/LaunchLoadingViewController.swift
+++ Q&A/LaunchLoadingViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LaunchLoadingViewController: UIViewController {
     
     var cloudKitManager = CloudKitManager()
+    var holderView = HolderView(frame: CGRect.zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,25 @@ class LaunchLoadingViewController: UIViewController {
                 }
             }
         }
-        
     }
+    
+    func animateLabel() {
+        // 1
+        holderView.removeFromSuperview()
+        view.backgroundColor = UIColor.blue
+        
+        // 2
+        let label: UILabel = UILabel(frame: view.frame)
+        label.textColor = UIColor.white
+        label.font = UIFont(name: "cochin", size: 170.0)
+        label.textAlignment = NSTextAlignment.center
+        label.text = "Q"
+        label.transform = label.transform.scaledBy(x: 0.25, y: 0.25)
+        view.addSubview(label)
+        
+        // 3
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.curveEaseInOut, animations: ({ label.transform = label.transform.scaledBy(x: 4.0, y: 4.0) }), completion: { finished in
+        })
+    }
+
 }
