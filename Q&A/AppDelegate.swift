a4a8eee44b5b6591d7629ece079862e315572751
Morty <mortmort5@gmail.com>
2017-04-18 20:31:47 -0600

    Changed color of status bar on the phone

--- Q&A/AppDelegate.swift
+++ Q&A/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        UIApplication.shared.statusBarStyle = .lightContent
         CKContainer.default().fetchUserRecordID { (recordID, error) in
             if let error = error {
                 print("Error with fetching recordID. \(error.localizedDescription)")
