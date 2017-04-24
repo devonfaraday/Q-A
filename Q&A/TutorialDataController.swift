af7b0a707676711c8b785ea8d4772033a0f9d86b
demickm <demickm@hotmail.com>
2017-04-24 14:17:02 -0600

    added tutorial storyboard and functionality needs images of tutorial added

---
+++ Q&A/TutorialDataController.swift
@@ -0,0 +1,68 @@
+//
+//  TutorialDataController.swift
+//  Q&A
+//
+//  Created by Demick McMullin on 4/24/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class TutorialDataController: NSObject, UIPageViewControllerDataSource {
+    
+    static let shared = TutorialDataController()
+    
+    var pageData: [UIImage] = []
+    let test1 = #imageLiteral(resourceName: "RedButton")
+    let test2 = #imageLiteral(resourceName: "GreenButton")
+ 
+    override init() {
+        super.init()
+        pageData = [test1, test2]
+        
+    }
+    
+    func viewControllerAtIndex(_ index: Int, storyboard: UIStoryboard) -> TutorialDataViewController? {
+        // Return the data view controller for the given index.
+        if (self.pageData.count == 0) || (index >= self.pageData.count) {
+            return nil
+        }
+        
+        // Create a new view controller and pass suitable data.
+        let dataViewController = storyboard.instantiateViewController(withIdentifier: "TutorialDataViewController") as? TutorialDataViewController
+        dataViewController?.tutorialImage = self.pageData[index]
+        return dataViewController
+    }
+    
+    func indexOfViewController(_ viewController: TutorialDataViewController) -> Int {
+        // Return the index of the given data view controller.
+       guard let tutorialImage = viewController.tutorialImage else {return 0}
+        return pageData.index(of: tutorialImage) ?? NSNotFound
+    }
+    
+    // MARK: - Page View Controller Data Source
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        var index = self.indexOfViewController(viewController as! TutorialDataViewController)
+        if (index == 0) || (index == NSNotFound) {
+            return nil
+        }
+        
+        index -= 1
+        return self.viewControllerAtIndex(index, storyboard: viewController.storyboard!)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        var index = self.indexOfViewController(viewController as! TutorialDataViewController)
+        if index == NSNotFound {
+            return nil
+        }
+        
+        index += 1
+        if index == self.pageData.count {
+            return nil
+        }
+        return self.viewControllerAtIndex(index, storyboard: viewController.storyboard!)
+    }
+    
+}
---
+++ Q&A/TutorialDataViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TutorialDataViewController.swift
+//  Q&A
+//
+//  Created by Demick McMullin on 4/24/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class TutorialDataViewController: UIViewController {
+
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var page: UIPageControl!
+    
+    var tutorialImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let index = TutorialDataController.shared.indexOfViewController(self)
+        guard let tutorialImage = tutorialImage else {return}
+        imageOutlet.image = tutorialImage
+        page.isEnabled = false
+        page.currentPageIndicatorTintColor = UIColor.blue
+        page.numberOfPages = TutorialDataController.shared.pageData.count
+        page.currentPage = index
+        page.isHidden = false
+    }
+
+
+}
